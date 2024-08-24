dce1750fc387161607e189942f3bb432980a8d45
Adrian-TJ <atrevino136@gmail.com>
2024-08-24 17:18:41 -0600

    Api estructure and middleware creation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from routes.auth.login import routes_auth
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+app.register_blueprint(routes_auth, url_prefix="/api")
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
+	load_dotenv()
 	app.run(host='0.0.0.0', port = 5000, debug=True)
---
+++ src/routes/auth/login.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+from routes.middleware.JWT import write_token
+
+routes_auth = Blueprint("routes_auth", __name__)
+
+@routes_auth.route("/login", methods=["POST"])
+def login():
+	data = request.get_json()
+	print(data)
+	if data.username == "Pedro":
+		return write_token(data=request.get_json())
+	
+	else:
+		response = jsonify({"message": "User not found"})
+		response.status_code = 404
+		return response
