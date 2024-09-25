27ea73f6d9ddbc7acfcc8e65e681616ff02ad559
Adrian-TJ <atrevino136@gmail.com>
2024-09-25 12:15:03 -0600

    Emergency changes

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ from os import getenv
 
 if __name__ == '__main__':
 	app = initialize_app()
-	app.run(host=getenv("API_HOST"), port = getenv("API_PORT"), debug=True)
+	app.run(host=getenv("API_HOST_PROD"), port = getenv("API_PORT_PROD"), debug=True)
