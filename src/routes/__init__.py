ee22b32ab3e1c7510bb5ea6e13880b664ae14322
Pedro <pedrosava01@gmail.com>
2024-09-25 12:44:22 -0600

    [flask][Attendance] : Attendance route with SP and documentation

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flasgger import Swagger
+
 
 def initialize_app():
-  app = Flask(__name__)
   load_dotenv()
+  app = Flask(__name__)
+  
+  swagger = Swagger(app, template={
+		"info": {
+			"title": "Caritas API",
+			"description": "REST API para Caritas de Monterrey",
+			"version": "1.0.1"
+			}
+  })
   
   from .auth import auth_routes 
   app.register_blueprint(auth_routes, url_prefix="/api/auth")
@@ -13,6 +23,7 @@ def initialize_app():
 
   from .attendace import attendance_routes
   app.register_blueprint(attendance_routes,url_prefix= "/api/attendance")
+  
   
   return app
 
--- src/routes/attendace/__init__.py
+++ src/routes/attendace/__init__.py
@@ -1,20 +1,80 @@
 from flask import Blueprint, request, jsonify
+from util.db_connection import DB
 
 attendance_routes = Blueprint("attendance", __name__)
 
 @attendance_routes.route("/attendance", methods=["POST"])
 def attendance():
+    """
+	Realiza el registro de asistencia de un usuario a un evento determinado.
+	---
+	parameters:
+		- in: body
+		  ID_USUARIO: ID_USER
+		  description: ID usuario
+		  schema:
+		  	type: object
+			required:
+				- USER ID
+				- EVENT ID
+			properties:
+				ID_USER:
+					type: int
+					description: ID del usuario
+					example: 1
+				ID_EVENT:
+					type: int
+					description: ID del evento al que se estará registrando
+					example: 1
+	responses:
+		200:
+		  description: Usuario registrado con éxito
+		  content:
+			application/json:
+			  schema:
+			  	type: object
+				properties:
+					message:
+						type: string
+						example: Success
+
+		400:
+		  description: Error en registrar Asistencia
+		  content:
+			application/json:
+			  schema:
+			  	type: object
+				properties:
+					message:
+						type: string
+						example: Error en conexión
+		
+		400:
+		  description: Evento o usuario no válido
+		  content:
+			application/json:
+			  schema:
+			  	type: object
+				properties:
+					message:
+						type: string
+						example: ID de usuario o evento no válido
+	"""
+
+
     data = request.get_json()
-    if not data:
-        return jsonify({"error": "Invalid data"}), 400
 
-    ID_USER = data.get("ID_USER")
-    ID_EVENT = data.get("ID_EVENT")
-
-    if ID_USER is None or ID_EVENT is None:
-        return jsonify({"error": "Falta ID del evento o de un Usuario Valido"}), 400
-    
-    if ID_USER == 1:
-        return jsonify({"message": "Gracias por registrarte Pedro"}), 200
+    if DB.eventAttendance(data["ID_USUARIO", data["ID_EVENTO"]]):
+        try:
+            response = jsonify({"message":"Usuario registrado con éxito"})
+            response.status_code = 200
+            return response
+        
+        except:
+            response = jsonify({"message":"Error en registrar usuario"})
+            response.status_code = 400
+            return response 
     else:
-        return jsonify({"error": "Error en el registro"}), 400
+        response = jsonify({"message":"Evento no encontrado"})
+        response.status_code = 400
+        return response
